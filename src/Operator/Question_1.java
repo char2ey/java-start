9a9095aabe1f918dc2f2df9911b05398079017aa
root <root@Char1ey>
2024-03-30 01:04:02 +0900

    Feat: Operator/Question_1

---
+++ src/Operator/Question_1.java
@@ -0,0 +1,16 @@
+package Operator;
+
+public class Question_1 {
+    public static void main(String[] args) {
+        int num1, num2, num3;
+        num1 = 10;
+        num2 = 20;
+        num3 = 30;
+
+        int sum = num1 + num2 + num3;
+        int average = (num1 + num2 + num3) / 3;
+
+        System.out.println("sum = " + sum);
+        System.out.println("average = " + average);
+    }
+}

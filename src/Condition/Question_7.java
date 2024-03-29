410ca9b2dcc9a81a9b91e48115acb769aa532a57
root <root@Char1ey>
2024-03-30 02:01:37 +0900

    Feat: Condition/Question_7

---
+++ src/Condition/Question_7.java
@@ -0,0 +1,15 @@
+package Condition;
+
+public class Question_7 {
+    public static void main(String[] args) {
+        int x;
+
+        x = 17;
+
+        int mod = x % 2;
+
+        String result = mod == 0 ? "짝수" : "홀수";
+
+        System.out.println(result);
+    }
+}

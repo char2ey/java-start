0ac0e867687d009017241a56f5bbe6dbfa4e4573
root <root@Char1ey>
2024-03-30 01:00:45 +0900

    Feat: Variable/Question_3

---
+++ src/Variable/Question_3.java
@@ -0,0 +1,11 @@
+package Variable;
+
+public class Question_3 {
+    public static void main(String[] args) {
+        long num = 10000000000L;
+        System.out.println("num = " + num);
+
+        boolean bool = true;
+        System.out.println("bool = " + bool);
+    }
+}
